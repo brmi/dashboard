eef51a04e0a525cef5d5c622849d164b864fd3f6
brmi <desireelenart@gmail.com>
2016-06-21 17:30:51 -0700

    update

--- thedash/static/js/main.js
+++ thedash/static/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    $(document).on('click', '.clickable-row', (function(){
+    $(document).on('click', '.clickable-row', (function(e){
+
         window.document.location = $(this).data("href");
     }));
 });
